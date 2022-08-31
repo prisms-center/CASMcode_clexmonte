29f41c18da3ba0f6ad252037164747c76c229574
bpuchala <bpuchala@users.noreply.github.com>
2022-08-31 09:05:45 -0400

    add kmc-lotto submodule

---
+++ include/casm/clexmonte/kmc/lotto.hh
@@ -0,0 +1,7 @@
+#ifndef CASM_clexmonte_kmc_lotto
+#define CASM_clexmonte_kmc_lotto
+
+#include "casm/clexmonte/kmc/lotto/rejection.hpp"
+#include "casm/clexmonte/kmc/lotto/rejection_free.hpp"
+
+#endif
